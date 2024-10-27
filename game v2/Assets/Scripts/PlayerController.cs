08e11cc7099a4cb43ae574b5a9b8f6f151cea153
Przemek031990 <95852239+Przemek031990@users.noreply.github.com>
2024-10-27 22:18:50 +0100

    kolizja z obiektami
    Zrobiona kolizja z obiektami + dodanie NPC ( jeszcze nie ma funkcjonalności )

--- game v2/Assets/Scripts/PlayerController.cs
+++ game v2/Assets/Scripts/PlayerController.cs
@@ -3,62 +3,6 @@ using System.Collections.Generic;
 using UnityEditor.UIElements;
 using UnityEngine;
 
-//public class PlayerController : MonoBehaviour
-//{
-//    public float moveSpeed;
-
-//    private bool isMoving;
-
-//    private Vector2 input;
-
-//    private Animator animator;
-
-//    private void Awake()
-//    {
-//        animator = GetComponent<Animator>();
-//        Debug.Log("Animator:" + animator);
-//    }
-
-//    private void Update()
-//    {
-//        if (!isMoving)
-//        {
-//            input.x = Input.GetAxisRaw("Horizontal");
-//            input.y = Input.GetAxisRaw("Vertical");
-
-//            if (input.x != 0) input.y = 0;
-
-//            if (input != Vector2.zero)
-//            {
-//                animator.SetFloat("moveX", input.x);
-//                animator.SetFloat("moveY", input.y);
-
-//                var targetPos = transform.position;
-//                targetPos.x += input.x;
-//                targetPos.y += input.y;
-
-//                StartCoroutine(Move(targetPos));
-//            }
-//        }
-
-//        animator.SetBool("isMoving", isMoving);
-
-
-//    }
-//    IEnumerator Move(Vector3 targetPos)
-//    {
-//        isMoving = true;
-
-//        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
-//        {
-//            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
-//            yield return null;
-//        }
-
-//        transform.position = targetPos;
-//        isMoving = false;
-//    }
-//}
 
 
 public class PlayerController : MonoBehaviour
@@ -72,6 +16,7 @@ public class PlayerController : MonoBehaviour
     private Animator animator;
 
     public LayerMask solidObjectsLayer;
+    public LayerMask interactablesLayer;
 
     private void Awake()
     {
@@ -106,7 +51,7 @@ public class PlayerController : MonoBehaviour
                     StartCoroutine(Move(targetPos));
 
 
-                StartCoroutine(Move(targetPos));
+                
             }
         }
 
@@ -129,7 +74,7 @@ public class PlayerController : MonoBehaviour
 
     private bool IsWalkable(Vector3 targetPos)
     {
-        if (Physics2D.OverlapCircle(targetPos, 0.2f, solidObjectsLayer) != null)
+        if (Physics2D.OverlapCircle(targetPos, 0.1f, solidObjectsLayer | interactablesLayer) != null)
         {
             return false;
         }
@@ -137,4 +82,3 @@ public class PlayerController : MonoBehaviour
     }
 
 }
-
