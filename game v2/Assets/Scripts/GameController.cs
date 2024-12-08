54f51bfe9cea752b92d32f1a611a2d58ee3353b5
marekfrychel <147609487+marekfrychel@users.noreply.github.com>
2024-12-08 10:55:42 +0100

    Revert "Attak oraz animacja chodzenia"
    This reverts commit a17cbf440f3ccb61bd4d4d5f12eefbfe97835b22.

--- game v2/Assets/Scripts/GameController.cs
+++ game v2/Assets/Scripts/GameController.cs
@@ -1,5 +1,10 @@
+// GameController to centralny skrypt zarz¹dzaj¹cy stanem gry, który okreœla,
+// w jakim trybie znajduje siê gra (swobodna eksploracja, dialog lub walka)
+// i odpowiednio przekazuje kontrolê do innych skryptów, np. do obs³ugi gracza lub dialogów.
+
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 // Wyliczenie stanów gry, aby ³atwo kontrolowaæ, czy gra jest w trybie eksploracji, dialogu, czy walki
@@ -32,7 +37,7 @@ public class GameController : MonoBehaviour
         // Obs³uguj logikê w zale¿noœci od stanu gry
         if (state == GameState.FreeRoam)
         {
-            // W trybie swobodnej eksploracji, Update() z PlayerController bêdzie obs³ugiwaæ ruch gracza
+            playerController.HandleUpdate();
         }
         else if (state == GameState.Dialog)
         {
@@ -44,3 +49,4 @@ public class GameController : MonoBehaviour
         }
     }
 }
+
--- game v2/Assets/Scripts/PlayerController.cs
+++ game v2/Assets/Scripts/PlayerController.cs
@@ -1,68 +1,96 @@
+// PlayerController obs³uguje ruchy gracza, w tym interakcje z otoczeniem oraz system animacji.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f; // Prêdkoœæ poruszania siê
+    public float moveSpeed; // Prêdkoœæ ruchu gracza
 
-    private Vector2 input; // Wektor wejœciowy (ruch)
-    private Rigidbody2D rb; // Komponent Rigidbody2D
-    private Animator animator; // Komponent Animatora
+    private bool isMoving;
+    private Vector2 input;
+    private Animator animator;
 
-    // Warstwy dla obiektów interaktywnych
+    // Warstwy wykorzystywane do sprawdzenia przeszkód i obiektów interaktywnych
+    public LayerMask solidObjectsLayer;
     public LayerMask interactableLayer;
 
     private void Awake()
     {
-        // Pobierz komponenty przypisane do obiektu
-        rb = GetComponent<Rigidbody2D>();
+        // Pobiera komponent Animator przypisany do tego obiektu
         animator = GetComponent<Animator>();
+    }
 
-        // SprawdŸ, czy Rigidbody2D jest przypisane
-        if (rb == null)
+    public void HandleUpdate()
+    {
+        // Obs³uguje wejœcia tylko, gdy gracz nie porusza siê
+        if (!isMoving)
         {
-            Debug.LogError("Rigidbody2D is missing from Player!");
+            // Pobiera wejœcia poziome i pionowe
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+
+            if (input.x != 0) input.y = 0; // Zapobiega ruchowi diagonalnemu
+
+            if (input != Vector2.zero)
+            {
+                // Ustawia kierunek animacji
+                animator.SetFloat("moveX", input.x);
+                animator.SetFloat("moveY", input.y);
+
+                // Ustalanie docelowej pozycji na podstawie kierunku
+                var targetPos = transform.position;
+                targetPos.x += input.x;
+                targetPos.y += input.y;
+
+                // Sprawdza, czy docelowa pozycja jest przejezdna
+                if (IsWalkable(targetPos))
+                    StartCoroutine(Move(targetPos)); // Rozpocznij ruch
+            }
         }
-    }
 
-    private void Update()
-    {
-        // Odczyt ruchu gracza z wejœcia
-        input.x = Input.GetAxisRaw("Horizontal");
-        input.y = Input.GetAxisRaw("Vertical");
+        animator.SetBool("isMoving", isMoving); // Aktualizacja stanu animacji
 
-        // Normalizacja wektora, aby ruch po skosie by³ równy
-        if (input.magnitude > 1)
-            input = input.normalized;
-
-        // Obs³uga animacji
-        animator.SetFloat("moveX", input.x);
-        animator.SetFloat("moveY", input.y);
-        animator.SetBool("isMoving", input.magnitude > 0);
-
-        // Obs³uga interakcji po naciœniêciu klawisza Z
         if (Input.GetKeyDown(KeyCode.Z))
-            Interact();
-    }
-
-    private void FixedUpdate()
-    {
-        // Przesuwanie postaci z u¿yciem Rigidbody2D
-        rb.velocity = input * moveSpeed;
+            Interact(); // Wywo³anie interakcji, jeœli naciœniêto Z
     }
 
     void Interact()
     {
         // Ustalanie kierunku interakcji
-        Vector3 facingDir = new Vector3(animator.GetFloat("moveX"), animator.GetFloat("moveY"));
-        Vector3 interactPos = transform.position + facingDir;
+        var facingDir = new Vector3(animator.GetFloat("moveX"), animator.GetFloat("moveY"));
+        var interactPos = transform.position + facingDir;
 
         Debug.DrawLine(transform.position, interactPos, Color.red, 1f);
 
-        // SprawdŸ, czy gracz dotyka obiektu interaktywnego
-        Collider2D collider = Physics2D.OverlapCircle(interactPos, 0.2f, interactableLayer);
+        // Sprawdzanie kolizji z obiektem interaktywnym
+        var collider = Physics2D.OverlapCircle(interactPos, 0.2f, interactableLayer);
         if (collider != null)
         {
             collider.GetComponent<Interactable>()?.Interact(); // Wywo³anie interakcji
         }
+    }
+
+    IEnumerator Move(Vector3 targetPos)
+    {
+        isMoving = true;
+
+        // Poruszanie gracza do docelowej pozycji
+        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        transform.position = targetPos;
+        isMoving = false;
+    }
+
+    private bool IsWalkable(Vector3 targetPos)
+    {
+        // Sprawdza, czy na docelowej pozycji nie ma przeszkód
+        return Physics2D.OverlapCircle(targetPos, 0.1f, solidObjectsLayer | interactableLayer) == null;
     }
 }
