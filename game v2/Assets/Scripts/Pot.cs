b536a3c4d9a4ebbb40139d76979981cab1c97f4a
marekfrychel <147609487+marekfrychel@users.noreply.github.com>
2025-01-07 18:45:42 +0100

    Hitboxy kierunkowe dla attacku, pot
    dodanie kierunkowych hitboxow dla attaku, dodanie ich do animacji, dodanie do animatora, zmiana ich sprite'a, dodanie pota, dodanie animacji pota, dodanie zarysu funkcji rozwalania pota

---
+++ game v2/Assets/Scripts/Pot.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pot : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Smash()
+    {
+        anim.SetBool("smash", true);
+    }
+}
